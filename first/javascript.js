b10f0637a87de0d8d86edea7df7f9dd4f36b93bc
NaGi77 <245479702@qq.com>
2016-04-10 02:35:12 +0800

    my first page
    = =偶然翻到了，留个纪念

---
+++ first/javascript.js
@@ -0,0 +1,26 @@
+alert("颜色可以改变哦！！！");
+/*function choosecolor(){
+	var color123="";
+	if(color123=1)
+	{ 
+		window.open('file:///D:/%E5%89%8D%E7%AB%AF/first/web1.html')
+	}
+	else if(color123=2)
+	{
+        window.open('file:///D:/%E5%89%8D%E7%AB%AF/first/web1.html')
+	}
+	else
+	{
+		window.open('file:///D:/%E5%89%8D%E7%AB%AF/first/web.html')
+	}
+}*/
+
+/*function choosecolor(){
+	var color123= "";
+	window.showModalDialog(color123,'open','status=no;scroll=no;resizable=no;help=no;dialogWidth=660px;dialogHeight=600px');
+}*/
+/*function choose()
+{
+	var text = document.getElementById("text");
+	text.className ="one";
+}*/
---
+++ first/save.php
@@ -0,0 +1,3 @@
+QAQ
+hello
+welcome
